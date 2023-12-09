916f2bd902fa75680903799a12788fb9f98749b2
Sabari K <sabari8956@gmail.com>
2023-12-09 19:29:02 +0530

    Models added POST

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username}"

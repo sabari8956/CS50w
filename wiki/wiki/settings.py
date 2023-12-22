afb5129a6b7fbcbb7607b0cd3c25024b6153e476
Sabari K <sabari8956@gmail.com>
2023-12-22 18:47:13 +0530

    changes

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["saabri8956.azurewebsites.net"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
